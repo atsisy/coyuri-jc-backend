b273981c2bd37818b0015dbcd3ad40dcdcc0eb7a
Akihiro Takai <at.sisy@gmail.com>
2017-05-21 22:06:05 +0900

    Banクラスを定義

---
+++ coyuri/define_ban.cpp
@@ -0,0 +1,18 @@
+﻿#include "ban.hpp"
+#include "values.hpp"
+
+Ban::Ban() {
+	for (int y = 1; y <= 9; y++) {
+		for (int x = 1; x <= 9; x++) {
+			/*
+			*裏で動く盤面変数はすべてEMPTYで初期化
+			*/
+			backend_ban[x - 1][y - 1] = EMPTY;
+			frontend_ban[x - 1][y - 1] = new Masu(x*IMAGE_SIDE, y*IMAGE_SIDE, IMAGE_SIDE, IMAGE_SIDE);
+			select_ban[x - 1][y - 1] = new Masu(x*IMAGE_SIDE, y*IMAGE_SIDE, IMAGE_SIDE, IMAGE_SIDE);
+
+			frontend_ban[x - 1][y - 1]->image(nullptr);
+			select_ban[x - 1][y - 1]->image(nullptr);
+		}
+	}
+}
